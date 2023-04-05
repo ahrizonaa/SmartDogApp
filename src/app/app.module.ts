2d8015f613d789f2d9a1bd7065f36bb2eb4fc225
missahrizona <princessssahri@gmail.com>
2023-04-04 23:06:18 -0500

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Tab1Page } from './tab1/tab1.page';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, Tab1Page],
+})
+export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,84 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { NgIf } from '@angular/common';
+import { NgxSliderModule, Options } from '@angular-slider/ngx-slider';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+declare const $: any;
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent, NgIf, NgxSliderModule],
 })
-export class Tab1Page {
-  constructor() {}
+export class Tab1Page implements OnInit, AfterViewInit {
+  @ViewChild('AudioInputContainer') audioInputContainer: any;
+  @ViewChild('ChatBubbleDiv') chatBubbleDiv: any;
+
+  isMicrophoneActive: boolean;
+  heartsensor: any;
+  speakerphone: any;
+  microphone: any;
+  sliderOptions: Options;
+
+  constructor() {
+    this.isMicrophoneActive = false;
+    this.heartsensor = {
+      IsAlertActive: false,
+    };
+    this.speakerphone = {
+      IsActive: false,
+      ActivityStream: new BehaviorSubject<boolean>(false),
+      ActiveSirenIcon: '/assets/siren-off.svg',
+    };
+    this.microphone = {
+      IsActive: false,
+      ActivityStream: new BehaviorSubject<boolean>(false),
+      Volume: 5,
+      chatBubbleDivHeight: 'auto',
+      chatBubbleIconSrc: '/assets/chat-single.svg',
+    };
+    this.sliderOptions = {
+      floor: 0,
+      ceil: 10,
+      vertical: true,
+    };
+
+    this.speakerphone.ActivityStream.subscribe((isActive: boolean) => {
+      this.speakerphone.IsActive = isActive;
+      this.speakerphone.ActiveSirenIcon = isActive
+        ? '/assets/siren-on.svg'
+        : '/assets/siren-off.svg';
+    });
+
+    this.microphone.ActivityStream.subscribe((isActive: boolean) => {
+      this.microphone.IsActive = isActive;
+      this.microphone.chatBubbleIconSrc = isActive
+        ? '/assets/chat-duo.svg'
+        : '/assets/chat-single.svg';
+    });
+  }
+
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      $('.chat-bubble').height($('.comm-animation-container').height());
+    }, 1000);
+  }
+
+  volumeChanged(event: any) {
+    this.microphone.Volume = event.detail.value;
+  }
+
+  AlarmSirenClicked(event: Event) {
+    this.speakerphone.ActivityStream.next(!this.speakerphone.IsActive);
+  }
+
+  MicrophoneClicked(event: Event) {
+    this.microphone.ActivityStream.next(!this.microphone.IsActive);
+  }
 }
